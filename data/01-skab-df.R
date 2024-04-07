bdb92fd69df9cd6f1c9118e83c69219a04284e5d
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-07 22:26:41 +0200

    Begynd refactor til loop og data fra df

--- data/01-skab-df.R
+++ data/01-skab-df.R
@@ -1,6 +1,7 @@
 library(httr)
 library(tidyverse)
 library(jsonlite)
+library(readxl)
 
 ## Definer URL der skal bruges
 url_DAWA <- "https://api.dataforsyningen.dk/afstemningsomraader"
@@ -19,3 +20,29 @@ test_response <- content(GET(test_url_DAWA))
 test_response$afstemningssted$navn
 test_response$nummer
 test_response$kommune$kode
+
+## Refactor til loop og med data fra DF
+
+csv_vind <- read_csv("data/downloads/vind.csv")
+colnames(csv_vind)
+head(csv_vind$"X (øst) koordinat \r\nUTM 32 Euref89")
+
+#### Indlæs excel fil med vindmølledata og omdøber kolonnenavne med det samme
+vind <- read_xlsx("data/downloads/vind.xlsx") %>% 
+  select(everything()) %>% 
+  rename(x_koord = "X (øst) koordinat \r\nUTM 32 Euref89",
+         y_koord = "Y (nord) koordinat \r\nUTM 32 Euref89")
+
+## Fjerner rækker med missing values i x og y koordinater
+vind <- vind %>% 
+  drop_na(x_koord, y_koord)
+
+vind[1, ]$x_koord
+
+nrow(vind)
+colnames(vind)
+
+for (i in 1:nrow(vind)) {
+  x_koord <- vind[i, ]$x_koord
+  y_koord <- vind[i, ]$y_koord
+}
