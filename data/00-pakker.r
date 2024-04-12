0db1240dc299614c2f9f01ba9de568ed4a932979
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-12 20:06:14 +0200

    tilføj samlet pakkeinstallation

---
+++ data/00-pakker.r
@@ -0,0 +1,29 @@
+## Her er liste over alle de pakker der skal bruges for at reproducere dette arbejde
+pakkeliste <- c(
+  "tidyverse",
+  "httr",
+  "jsonlite",
+  "readxl"
+)
+
+## Tjekker om alle pakker fra liste er i de installerede pakker
+mangler_pakker <- pakkeliste[!(pakkeliste %in% installed.packages()[,"Package"])]
+
+## Installerer pakker hvis de mangler
+if (length(mangler_pakker) > 0) {
+  install.packages(mangler_pakker, dependencies = TRUE)
+}
+
+## Indlæser alle pakker
+for (pakke.i in pakkeliste) {
+  suppressPackageStartupMessages(
+    library(
+      package.i,
+      character.only = TRUE
+      )
+  )
+}
+
+
+## Dropper midlertidig variabel
+rm(pakkeliste, mangler_pakker)
