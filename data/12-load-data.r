1385d8418c1f7191d584b022ed23af5fad4db4af
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-13 15:34:55 +0200

    begynd data load

--- data/12-load-data.r
+++ data/12-load-data.r
@@ -1,3 +1,14 @@
 library(tidyverse)
 
-## Indlæser dataframe hvis den er skabt tidligere
+## Indlæser csv data over vindmøllernes afstemningssteder
+vind_stemmesteder <- read_csv("data/downloads/mine_data/vind_steder.csv")
+
+## Indlæser stemmedata fra valgdatabasen
+stemmer <- read_csv2("data/downloads/ValgData.csv")
+
+## Indlæser geografisk opgørelse fra valgdatabasen
+geografi <- read_csv2("data/downloads/Geografi.csv")
+
+## Indlæser referenceopgørelse over valg og id fra valgdatabsen
+valg <- read.csv2("data/downloads/Valg.csv")
+
