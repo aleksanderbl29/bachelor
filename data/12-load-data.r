3a17343d119225d6f83a78184c95c4ac87027005
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-16 00:03:44 +0200

    Begynd behandling af valgdata

--- data/12-load-data.r
+++ data/12-load-data.r
@@ -4,11 +4,19 @@ library(tidyverse)
 vind_stemmesteder <- read_csv("data/downloads/mine_data/vind_steder.csv")
 
 ## Indlæser stemmedata fra valgdatabasen
-stemmer <- read_csv2("data/downloads/ValgData.csv")
+import_stemmer <- read_csv2("data/downloads/ValgData.csv")
 
 ## Indlæser geografisk opgørelse fra valgdatabasen
 geografi <- read_csv2("data/downloads/Geografi.csv")
 
 ## Indlæser referenceopgørelse over valg og id fra valgdatabsen
-valg <- read.csv2("data/downloads/Valg.csv")
+import_valg <- read.csv2("data/downloads/Valg.csv")
 
+# lokalplaner <- read.csv("data/planer.csv")
+# view(lokalplaner)
+# unique(lokalplaner$planstatus)
+# 
+# 
+# lokalplaner_udsnit <- read.csv("data/planer_udsnit.csv")
+# view(lokalplaner_udsnit)
+# unique(lokalplaner_udsnit$planstatus)
--- data/13-dst.r
+++ data/13-dst.r
@@ -1,10 +1,14 @@
+source("data/12-load-data.r")
+
 library(tidyverse)
+library(modelsummary)
 
 ## Finder informationer fra valgoversigt
-exists("valg")
+exists("import_valg")
+exists("import_stemmer")
+
 # Formaterer variable
-colnames(valg)
-valg <- valg %>% 
+valg <- import_valg %>% 
   select(Valgdag, ValgId) %>% 
   rename(valg_dato = Valgdag,
          valg_id = ValgId) %>% 
@@ -15,23 +19,45 @@ head(valg)
 gem_kolonner <- c("gruppe", "valgsted_id", "kreds_nr", "storkreds_nr", "landsdel_nr")
 
 ## Sammenlægger valg til tidy data
-tidy_stemmer <- stemmer %>% 
-  select(!ends_with(c("Afgivne stemmer", "Andre ugyldige stemmer", "Gyldige stemmer", "Blanke stemmer", "Stemmeberettigede"))) %>% 
+stemmer <- import_stemmer %>% 
+  # select(!ends_with(c("Afgivne stemmer", "Andre ugyldige stemmer", "Gyldige stemmer", "Blanke stemmer", "Stemmeberettigede"))) %>% 
+  select(!ends_with(c("Afgivne stemmer", "Andre ugyldige stemmer", "Blanke stemmer"))) %>% 
   rename(gruppe = Gruppe,
          valgsted_id = ValgstedId,
          kreds_nr = KredsNr,
          storkreds_nr = StorKredsNr,
          landsdel_nr = LandsdelsNr)
 
-colnames(tidy_stemmer)
+stemmer[] <- lapply(stemmer, as.character)
 
-partinavne <- tidy_stemmer %>% select(!any_of(gem_kolonner)) %>% colnames() %>% list()
-partinavne
-
-partinavne <- gsub('[KV2013957 -]', '', partinavne)
-
+# rm(import_stemmer, import_valg)
 
 head(stemmer, 2)
+
+alle_stemmer <- stemmer %>% 
+  select(any_of(gem_kolonner), starts_with("KV")) %>% 
+  pivot_longer(cols = starts_with("KV"),
+               names_to = "valg",
+               values_to = "stemmer") %>% 
+  separate(col = valg, into = c("valg", "parti"), sep = " - ") %>% 
+  mutate(partinavn = case_when(parti == "A" ~ "Socialdemokratiet",
+                               parti == "B" ~ "Radikale venstre",
+                               parti == "C" ~ "Det Konservative Folkeparti",
+                               parti == "D" ~ "Nye Borgerlige",
+                               parti == "F" ~ "Socialistisk Folkeparti",
+                               parti == "G" ~ "Veganerpartiet",
+                               parti == "I" ~ "Liberal Alliance",
+                               parti == "J" ~ "Junibevægelsen",
+                               parti == "K" ~ "Kristendemokraterne",
+                               parti == "L" ~ "Lokalliste"))
+         # partinavn = case_when(parti == "M" & (valg == "KV20")))
+
+head(alle_stemmer)
+unique(alle_stemmer$parti)
+unique(alle_stemmer$partinavn)
+unique(alle_stemmer$valg)
+
+datasummary_crosstab(data = alle_stemmer, formula = valg~partinavn)
 
 ## Stemmer for Socialdemokratiet
 a_stemmer <- stemmer %>% 
@@ -44,7 +70,16 @@ colnames(a_stemmer)
 head(a_stemmer, 3)
 
 
+partistemmer <- function(partibogstav, output_df, gemme_kolonner)
+
   
 
 colnames(stemmer)
 head(stemmer, 2)
+
+
+
+
+
+
+
