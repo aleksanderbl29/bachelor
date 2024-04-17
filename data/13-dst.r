f8d684daad0e56cbabc34b88746f640909b8da08
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-17 16:47:34 +0200

    tjek for antal obs variabel

--- data/13-dst.r
+++ data/13-dst.r
@@ -8,10 +8,10 @@ exists("import_valg")
 exists("import_stemmer")
 
 # Formaterer variable
-valg <- import_valg %>% 
-  select(Valgdag, ValgId) %>% 
+valg <- import_valg %>%
+  select(Valgdag, ValgId) %>%
   rename(valg_dato = Valgdag,
-         valg_id = ValgId) %>% 
+         valg_id = ValgId) %>%
   mutate(valg_dato = ymd(valg_dato))
 
 head(valg)
@@ -19,8 +19,8 @@ head(valg)
 gem_kolonner <- c("gruppe", "valgsted_id", "kreds_nr", "storkreds_nr", "landsdel_nr")
 
 ## Sammenlægger valg til tidy data
-stemmer <- import_stemmer %>% 
-  select(!ends_with(c("Afgivne stemmer", "Andre ugyldige stemmer", "Blanke stemmer"))) %>% 
+stemmer <- import_stemmer %>%
+  select(!ends_with(c("Afgivne stemmer", "Andre ugyldige stemmer", "Blanke stemmer"))) %>%
   rename(gruppe = Gruppe,
          valgsted_id = ValgstedId,
          kreds_nr = KredsNr,
@@ -33,12 +33,12 @@ stemmer[] <- lapply(stemmer, as.character)
 
 head(stemmer, 2)
 
-alle_stemmer <- stemmer %>% 
-  select(any_of(gem_kolonner), starts_with("KV")) %>% 
+alle_stemmer <- stemmer %>%
+  select(any_of(gem_kolonner), starts_with("KV")) %>%
   pivot_longer(cols = starts_with("KV"),
                names_to = "valg",
-               values_to = "stemmer") %>% 
-  separate(col = valg, into = c("valg", "parti"), sep = " - ") %>% 
+               values_to = "stemmer") %>%
+  separate(col = valg, into = c("valg", "parti"), sep = " - ") %>%
   mutate(partinavn = case_when(parti == "A" ~ "Socialdemokratiet",
                                parti == "B" ~ "Radikale venstre",
                                parti == "C" ~ "Det Konservative Folkeparti",
@@ -63,21 +63,21 @@ unique(alle_stemmer$valg)
 
 alle_nrow <- nrow(alle_stemmer)
 
-lang_alle_stemmer <- alle_stemmer %>% 
-  select(!partinavn) %>% 
+lang_alle_stemmer <- alle_stemmer %>%
+  select(!partinavn) %>%
   pivot_wider(names_from = parti, values_from = stemmer)
 
-noget_mere_stemmer <- alle_stemmer %>% 
+noget_mere_stemmer <- alle_stemmer %>%
   drop_na(partinavn)
 unique(noget_mere_stemmer$parti)
 nrow(noget_mere_stemmer)
 
-kmd_stemmer <- stemmer %>% 
-  select(any_of(gem_kolonner), starts_with("KV")) %>% 
+kmd_stemmer <- stemmer %>%
+  select(any_of(gem_kolonner), starts_with("KV")) %>%
   pivot_longer(cols = starts_with("KV"),
                names_to = "valg",
-               values_to = "stemmer") %>% 
-  separate(col = valg, into = c("valg", "parti"), sep = " - ") %>% 
+               values_to = "stemmer") %>%
+  separate(col = valg, into = c("valg", "parti"), sep = " - ") %>%
   mutate(partinavn = case_when(parti == "A" ~ "Socialdemokratiet",
                                parti == "V" ~ "Venstre, Danmarks Liberale Parti",
                                parti == "C" ~ "Det Konservative Folkeparti",
@@ -100,7 +100,7 @@ kmd_stemmer <- stemmer %>%
                                parti == "N" ~ "Nyt Odsherred",
                                parti == "W" ~ "Bornholmerlisten",
                                parti == "Stemmeberettigede" ~ "Stemmeberettigede",
-                               parti == "Gyldige stemmer" ~ "Gyldige stemmer")) %>% 
+                               parti == "Gyldige stemmer" ~ "Gyldige stemmer")) %>%
   drop_na(partinavn)
 
 kmd_nrow <- nrow(kmd_stemmer)
@@ -111,9 +111,9 @@ unique(kmd_stemmer$valg)
 
 kmd_nrow - alle_nrow
 
-na_stemmer <- alle_stemmer %>% 
+na_stemmer <- alle_stemmer %>%
   filter(is.na(partinavn))
-  
+
 na_nrow <- nrow(na_stemmer)
 
 na_nrow - alle_nrow
@@ -121,11 +121,11 @@ na_nrow - alle_nrow
 datasummary_crosstab(data = alle_stemmer, formula = valg~partinavn)
 
 ## Stemmer for Socialdemokratiet
-a_stemmer <- stemmer %>% 
-  select(any_of(gem_kolonner), ends_with("A")) %>% 
+a_stemmer <- stemmer %>%
+  select(any_of(gem_kolonner), ends_with("A")) %>%
   pivot_longer(cols = starts_with("KV"),
                names_to = "valg",
-               values_to = "stemmer") %>% 
+               values_to = "stemmer") %>%
   mutate(valg = str_remove(valg, " - A"))
 colnames(a_stemmer)
 head(a_stemmer, 3)
@@ -133,14 +133,10 @@ head(a_stemmer, 3)
 
 partistemmer <- function(partibogstav, output_df, gemme_kolonner)
 
-  
+
 
 colnames(stemmer)
 head(stemmer, 2)
 
-
-
-
-
-
-
+antal_obs_samlet
+exists(antal_obs_samlet)
