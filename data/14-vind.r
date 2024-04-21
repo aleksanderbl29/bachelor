ef60d958dc51d20d41df15a4fae0d3f5643d34a6
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-21 12:16:49 +0200

    objekter med valgperioder til tildeling af treatment

--- data/14-vind.r
+++ data/14-vind.r
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+options(scipen = 999)
+
 source("data/00-pakker.r")
 
 ## Indlæser alle data
@@ -12,4 +14,24 @@ source("data/13-dst.r")
 expect_true(exists("vind_stemmesteder"))
 expect_true(exists("valg"))
 
+## Fjerner ting der ikke skal bruges til udarbejdelse af funktioner
+rm(a_stemmer, alle_stemmer, geografi, import_stemmer, import_valg,
+   kmd_stemmer, noget_mere_stemmer, lang_alle_stemmer, na_stemmer,
+   stemmer, alle_nrow, gem_kolonner, kmd_nrow, na_nrow, partistemmer)
+
 vind_stemmesteder
+valg
+
+kv2001 <- valg$valg_dato[3]
+vp_01_05 <- as.interval(as.period(valg$valg_dato[4] - valg$valg_dato[3]), start = valg$valg_dato[3])
+vp_05_09 <- as.interval(as.period(valg$valg_dato[1] - valg$valg_dato[4]), start = valg$valg_dato[4])
+
+vp_01_05
+vp_05_09
+vp_09_13
+vp_13_17
+vp_17_21
+
+vind_treatment <- vind_stemmesteder %>% 
+  select(everything()) %>% 
+  mutate()
