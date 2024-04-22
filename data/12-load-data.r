496d3bc32aaad07304b22927b7af0fc5967d39b3
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-22 22:24:59 +0200

    fjern lokalplan loading

--- data/12-load-data.r
+++ data/12-load-data.r
@@ -5,18 +5,10 @@ vind_stemmesteder <- read_csv("data/downloads/mine_data/vind_steder.csv")
 
 ## Indlæser stemmedata fra valgdatabasen
 import_stemmer <- read_csv2("data/downloads/ValgData.csv")
+# import_stemmer <- read_csv2("data/downloads/udforskning/alle-kv-1-divergens/ValgData.csv")
 
 ## Indlæser geografisk opgørelse fra valgdatabasen
 geografi <- read_csv2("data/downloads/Geografi.csv")
 
 ## Indlæser referenceopgørelse over valg og id fra valgdatabsen
 import_valg <- read.csv2("data/downloads/Valg.csv")
-
-# lokalplaner <- read.csv("data/planer.csv")
-# view(lokalplaner)
-# unique(lokalplaner$planstatus)
-# 
-# 
-# lokalplaner_udsnit <- read.csv("data/planer_udsnit.csv")
-# view(lokalplaner_udsnit)
-# unique(lokalplaner_udsnit$planstatus)
