087529e393f653a8841b95e1e58c4b420fe2ed5d
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-22 22:25:13 +0200

    strukturer analyse fil

---
+++ data/20-analyse.r
@@ -0,0 +1,10 @@
+source("data/00-pakker.r")
+
+## Sikrer at alt nødvendigt forarbejde er gjort
+expect_true(exists("analyse_data"))
+
+## Indlæser ols fil
+source("data/21-ols.r")
+
+## Indlæser FE fil
+source("data/22-fixedeffects.r")
