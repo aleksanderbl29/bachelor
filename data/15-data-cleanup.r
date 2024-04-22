711ebcc26b375b522f08cf885ca1c771ace269bd
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-22 22:27:07 +0200

    fjern variable der ikke skal bruges

---
+++ data/15-data-cleanup.r
@@ -0,0 +1,36 @@
+
+analyse_data <- analyse_data %>% 
+  select(!c("1", "2", "3", "4", "5", "6", "0", "01", "02", "03", "04", "05", "06", "7", "K1", "K2", "T", "W", "Æ", "Å1"))
+
+
+
+
+
+
+rm(alle_stemmer,
+   geografi,
+   kmd_stemmer,
+   vind_treatment,
+   vind_stemmesteder,
+   kommunalvalg,
+   lang_alle_stemmer,
+   lang_gruppe_steder,
+   manuel_distinct_analyse_data,
+   na_stemmer,
+   stemmer,
+   treatment,
+   treatment_list)
+
+rm(alle_nrow,
+   gem_kolonner,
+   kmd_nrow,
+   na_nrow)
+
+rm(kv01_treatment,
+   kv05_treatment,
+   kv09_treatment,
+   kv13_treatment,
+   kv17_treatment,
+   kv21_treatment)
+
+rm(import_stemmer, import_valg)
