4cdc005366b1517155e7173c24c785dd30c73f55
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-04-23 19:14:48 +0200

    lav model til objekt

--- data/22-fixedeffects.r
+++ data/22-fixedeffects.r
@@ -37,4 +37,4 @@ msummary(list(m1, m2, m3, m4), stars = sign_stjerner)
 msummary(list(m1, m2, m3, m4, m5, m6), stars = sign_stjerner)
 
 ## Skab reg tbl 1
-reg_tbl1 <- msummary(list(m1, m2, m3, m4, m5, m6), stars = sign_stjerner)
+reg_tbl1 <- msummary(list(m1, m2, m3, m4, m5, m6), stars = sign_stjerner, output = "gt")
