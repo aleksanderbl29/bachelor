8621b971236da44b3797bce7c4f2a7c3fc201481
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-07 15:24:55 +0200

    chore: Update code files and remove unused code

--- parts/.vdoc.r
+++ parts/.vdoc.r
@@ -8,6 +8,12 @@
 #
 #
 #
+#| tbl-cap: "Regressionstabel med gt pakken"
+#| label: tbl-reg-1
+library(tidyverse)
+library(gt)
+print("HER MANGLER VIDST NOGET")
+# gt_reg_tbl |> opt_stylize(style = 1, color = "pink", add_row_striping = TRUE) %>% tab_source_note("Data er egen tilvirkning, DST og ENS") %>% tab_footnote("Her er en fodnote") # %>% gt::as_latex()
 #
 #
 #
@@ -16,7 +22,19 @@
 #
 #
 #
-sign_stjerner
+#
+#
+#
+#
+#
+#
+#| tbl-cap: "Regressionstabel med gt pakken"
+#| label: tbl-reg-html
+library(tidyverse)
+print("HER MANGLER VIDST OGSÅ NOGET")
+# gt_reg_tbl %>% opt_stylize(style = 1, color = "pink", add_row_striping = TRUE) %>% tab_source_note(source_note = md("Data er egen tilvirkning, *DST* og *ENS*")) %>% gt::as_raw_html()
+#
+#
 #
 #
 #
