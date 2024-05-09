2b7d333a22a7d840eb44429ac1e88c92b0597ff2
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-09 18:49:10 +0200

    fjern sletning

--- data/18-data-cleanup.r
+++ data/18-data-cleanup.r
@@ -1,28 +1,13 @@
+## Tjek at antal obs er passende
+
+
+
 ## Skriver analyse data til replikation.csv
 write_excel_csv(analyse_data, "data/downloads/mine_data/replikation.csv")
 
 # analyse_data <- analyse_data %>%
 #   select(!c("1", "2", "3", "4", "5", "6", "0", "01", "02", "03", "04", "05", "06", "7", "K1", "K2", "T", "W", "Æ", "Å1"))
 
-rm(alle_stemmer,
-   geografi,
-   vind_treatment,
-   vind_stemmesteder,
-   kommunalvalg,
-   lang_alle_stemmer,
-   lang_gruppe_steder,
-   stemmer,
-   treatment,
-   treatment_list)
 
-rm(alle_nrow,
-   gem_kolonner)
-
-rm(kv01_treatment,
-   kv05_treatment,
-   kv09_treatment,
-   kv13_treatment,
-   kv17_treatment,
-   kv21_treatment)
 
 
