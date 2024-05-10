4e66b70eee834da7fe205ef857fb1648ffa6f9e8
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-10 22:31:49 +0200

    omdøb kolonner til brugbare navne

--- data/12-load-data.r
+++ data/12-load-data.r
@@ -5,10 +5,13 @@ vind_stemmesteder <- read_csv("data/downloads/mine_data/vind_steder.csv")
 
 ## Indlæser stemmedata fra valgdatabasen
 import_stemmer <- read_csv2("data/downloads/ValgData.csv")
-# import_stemmer <- read_csv2("data/downloads/udforskning/alle-kv-1-divergens/ValgData.csv")
+# import_stemmer <- read_csv2("data/downloads/udforskning/alle-kv-1-øko-baggrund/ValgData.csv")
 
 ## Indlæser geografisk opgørelse fra valgdatabasen
-geografi <- read_csv2("data/downloads/Geografi.csv")
+geografi <- read_csv2("data/downloads/Geografi.csv") %>% 
+  rename(kommunenavn = "Kommune navn",
+         valgsted_id = "Valgsted Id",
+         kommunenr = "KommuneNr")
 
 ## Indlæser referenceopgørelse over valg og id fra valgdatabsen
 import_valg <- read.csv2("data/downloads/Valg.csv")
