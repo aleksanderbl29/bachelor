355145ed3b3950502fab71e4b217c03087025a41
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-13 02:30:14 +0200

    skriv til rds

--- data/11-skab-df.r
+++ data/11-skab-df.r
@@ -107,7 +107,7 @@ vind_steder <- vind_steder %>%
 
 head(vind_steder)
 write_excel_csv(vind_steder, "data/downloads/mine_data/vind_steder.csv")
-write_rds(vind_steder, "data/rep_data/vind_steder.rds")
+write_rds(vind_steder, "data/rep_data/11_vind_steder.rds")
 
 ## Fjern objekter der ikke skal bruges videre
 rm(loop_tid, sluttid, starttid, vind, vind_steder,
