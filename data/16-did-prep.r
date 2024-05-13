62f24dbf67603317bc08e0252b9aab097fe0be16
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-13 02:34:10 +0200

    skab rullende treatment

--- data/16-did-prep.r
+++ data/16-did-prep.r
@@ -1,11 +1,23 @@
-## Forbered til dynamisk did estimation
+library(tidyverse)
 
+## Indlæser analyse data
+analyse_data <- read_rds("data/rep_data/15_analyse_data.rds")
+
+## Laver dynamisk tilslutningsvariabel
 analyse_data <- analyse_data %>% 
-  mutate(dyn_did_t1 = case_when(kv01 == 1 & ))
+  select(everything()) %>%
+  mutate(tilslutning_aar = case_when(kv09 == 1 & valg_id == "kv09" ~ 1,
+                                     kv09 == 1 & valg_id != "kv09" ~ (((valgaar - 2009) / 4) + 1),
+                                     kv13 == 1 & valg_id == "kv13" ~ 1,
+                                     kv13 == 1 & valg_id != "kv13" ~ (((valgaar - 2013) / 4) + 1),
+                                     kv17 == 1 & valg_id == "kv17" ~ 1,
+                                     kv17 == 1 & valg_id != "kv17" ~ (((valgaar - 2017) / 4) + 1),
+                                     kv21 == 1 & valg_id == "kv21" ~ 1,
+                                     kv21 == 1 & valg_id != "kv21" ~ (((valgaar - 2021) / 4) + 1),
+                                     .default = 0))
 
+## Gemmer analyse data
+write_rds(analyse_data, "data/rep_data/16_analyse_data.rds")
 
-
-
-
-analyse_data <- analyse_data %>%
-  select(!c("kv01", "kv05", "kv09", "kv13", "kv17", "kv21"))
+## Rydder miljøet
+rm(list = ls())
