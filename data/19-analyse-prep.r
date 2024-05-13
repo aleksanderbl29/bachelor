cdd16446da87d0da2675a843140c0c1353fcd796
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-13 02:34:17 +0200

    gem og ryd

--- data/19-analyse-prep.r
+++ data/19-analyse-prep.r
@@ -2,5 +2,9 @@ library(tidyverse)
 library(logger)
 library(testthat)
 
-analyse_data <- read_rds("data/rep_data/15_analyse_data.rds")
+## Gemmer analyse data
+analyse_data <- read_rds("data/rep_data/16_analyse_data.rds")
 write_rds(analyse_data, "data/rep_data/rep_analyse_data.rds")
+
+## Rydder miljøet
+rm(list = ls())
