6b48e8f47c442e4e5f9ba16a03bd6700d1d6a007
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-13 02:30:23 +0200

    læs fra rds

--- data/12-load-data.r
+++ data/12-load-data.r
@@ -1,7 +1,8 @@
 library(tidyverse)
 
 ## Indlæser csv data over vindmøllernes afstemningssteder
-vind_stemmesteder <- read_csv("data/downloads/mine_data/vind_steder.csv")
+# vind_stemmesteder <- read_csv("data/downloads/mine_data/vind_steder.csv")
+vind_stemmesteder <- read_rds("data/rep_data/11_vind_steder.rds")
 
 ## Indlæser stemmedata fra valgdatabasen
 import_stemmer <- read_csv2("data/downloads/ValgData.csv")
