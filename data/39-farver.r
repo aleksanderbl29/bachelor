81b7032cd898f43365159de3c1cb4e25cc8207e3
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-15 13:38:01 +0200

    log indlæsning

--- data/39-farver.r
+++ data/39-farver.r
@@ -1,3 +1,5 @@
 library(tidyverse)
+library(logger)
 
+log_info("Skaber farvepalette til valg")
 valg_farver <-  c("KV2009" = "#E69F00", "KV2013" = "#56B4E9", "KV2017" = "#009E73", "KV2021" = "#F0E442")
