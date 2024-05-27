4a1060d89648c0a326fdd848ea2f2d3dbe70d232
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-27 14:03:00 +0200

    mere leg

--- data/88-leg-med-regres.r
+++ data/88-leg-med-regres.r
@@ -1,47 +1,59 @@
 library(fixest)
 library(tidyverse)
 library(modelsummary)
+library(bacondecomp)
+
+dat4 = data.frame(
+  id = rep(1:3, times = 10),
+  tt = rep(1:10, each = 3)
+) |>
+  within({
+    D = (id == 2 & tt >= 5) | (id == 3 & tt >= 8)
+    btrue = ifelse(D & id == 3, 4, ifelse(D & id == 2, 2, 0))
+    y = id + 1 * tt + btrue * D
+  })
+
+# Optional: custom ggplot2 theme
+theme_set(
+  theme_linedraw() +
+    theme(
+      panel.grid.minor = element_line(linetype = 3, linewidth = 0.1),
+      panel.grid.major = element_line(linetype = 3, linewidth = 0.1)
+    )
+)
+
+ggplot(dat4, aes(x = tt, y = y, col = factor(id))) +
+  geom_point() + geom_line() +
+  geom_vline(xintercept = c(4.5, 7.5), lty = 2) +
+  scale_x_continuous(breaks = scales::pretty_breaks()) +
+  labs(x = "Time variable", y = "Outcome variable", col = "ID")
+
+feols(y ~ D | id + tt, dat4)
+
+rbind(
+  dat4 |> subset(id %in% c(1,2)) |> transform(role = ifelse(id==2, "Treatment", "Control"), comp = "1.1. Early vs Untreated"),
+  dat4 |> subset(id %in% c(1,3)) |> transform(role = ifelse(id==3, "Treatment", "Control"), comp = "1.2. Late vs Untreated"),
+  dat4 |> subset(id %in% c(2,3) & tt<8) |> transform(role = ifelse(id==2, "Treatment", "Control"), comp = "2.1. Early vs Untreated"),
+  dat4 |> subset(id %in% c(2:3) & tt>4) |> transform(role = ifelse(id==3, "Treatment", "Control"), comp = "2.2. Late vs Untreated")
+) |>
+  ggplot(aes(tt, y, group = id, col = factor(id), lty = role)) +
+  geom_point() + geom_line() + 
+  facet_wrap(~comp) +
+  scale_x_continuous(breaks = scales::pretty_breaks()) +
+  scale_linetype_manual(values = c("Control" = 5, "Treatment" = 1)) +
+  labs(x = "Time variable", y = "Ouroleome variable", col = "ID", lty = "Role")
+
+(bgd = bacon(y ~ D, dat4, id_var = "id", time_var = "tt"))
+
+(bgd_wm = weighted.mean(bgd$estimate, bgd$weight))
 
 
-m1 <- feols(red_pct ~ ny_tilsluttet | valgsted_id + valg,
-            data = analyse_data)
+ggplot(bgd, aes(x = weight, y = estimate, shape = type, col = type)) +
+  geom_hline(yintercept = bgd_wm, lty  = 2) +
+  geom_point(size = 3) +
+  labs(
+    x = "Weight", y = "Estimate", shape = "Type", col = "Type",
+    title = "Bacon-Goodman decomposition example",
+    caption = "Note: The horizontal dotted line depicts the full TWFE estimate."
+  )
 
-m2 <- feols(blue_pct ~ ny_tilsluttet | valgsted_id + valg,
-            data = analyse_data)
-
-m3 <- feols(red ~ ny_tilsluttet | valgsted_id + valg,
-            data = analyse_data)
-
-m4 <- feols(blue ~ ny_tilsluttet | valgsted_id + valg,
-            data = analyse_data)
-
-
-m5_data <- analyse_data %>%
-  group_by(valgsted_id) %>%
-  mutate(ny_tilsluttet_within = ny_tilsluttet - mean(ny_tilsluttet),
-         red_within = red - mean(red)) %>%
-  ungroup() %>%
-  drop_na(red_within) %>%
-  drop_na(ny_tilsluttet)
-
-m5 <- m5_data %>% mutate(ny_tilsluttet = ny_tilsluttet_within) %>%
-  lm(red_within ~ ny_tilsluttet, data = ., na.action = na.omit)
-m6 <- lm(red_within ~ ny_tilsluttet, data = m5_data)
-
-msummary(list(m3, m4), stars = sign_stjerner)
-msummary(list(m1, m2), stars = sign_stjerner)
-msummary(list(m1, m2, m3, m4), stars = sign_stjerner)
-msummary(list(m1, m2, m3, m4, m5, m6), stars = sign_stjerner)
-
-## Skab reg tbl 1
-modeller_tbl_1 <- list("Rød blok pct" = m1,"Blå blok pct" = m2,"Rød blok" = m3,"Blå blok" = m4)
-gt_reg_tbl <- msummary(modeller_tbl_1, stars = sign_stjerner, output = "gt", gof_map = c("nobs", "r.squared", "adj.r.squared", "rmse", "vcov.type", "FE: valgsted_id", "FE: valg"), coef_rename = TRUE)
-gt_reg_tbl
-
-gt_reg_tbl  %>%  opt_stylize(style = 1, color = "pink", add_row_striping = FALSE) # %>% gt::as_latex()
-
-kable_reg_tbl <- msummary(modeller_tbl_1, stars = sign_stjerner, output = "kableExtra", gof_map = c("nobs", "r.squared", "adj.r.squared", "rmse", "vcov.type", "FE: valgsted_id", "FE: valg"), coef_rename = TRUE)
-kable_reg_tbl
-
-markdown_reg_tbl <- msummary(modeller_tbl_1, stars = sign_stjerner, output = "markdown", gof_map = c("nobs", "r.squared", "adj.r.squared", "rmse", "vcov.type", "FE: valgsted_id", "FE: valg"), coef_rename = TRUE)
-markdown_reg_tbl
