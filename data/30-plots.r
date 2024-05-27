28a648d26a6c863d36862ff9f711c18f5ef61906
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-27 14:05:59 +0200

    idlæs plot filer

--- data/30-plots.r
+++ data/30-plots.r
@@ -1,7 +1,13 @@
 library(tidyverse)
 
-## Indlæser farver
+## Indlæser farver og skrifttype
 source("data/39-farver.r")
 
 ## Map plots
 source("data/31-maps.r")
+
+## DID plots
+source("data/32-did-plots.r")
+
+## Stemmeplots
+source("data/33-stemme-plots.r")
