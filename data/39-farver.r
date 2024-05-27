4b6e2505cc988ac2d672ae59864db59589e45e5d
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-27 13:59:58 +0200

    partifarver

--- data/39-farver.r
+++ data/39-farver.r
@@ -6,3 +6,14 @@ valg_farver <- c("KV2009" = "#E69F00",
                  "KV2013" = "#56B4E9",
                  "KV2017" = "#009E73",
                  "KV2021" = "#F0E442")
+
+log_info("Skaber farvepalette til partier")
+partier_farver <- c("Socialdemokratiet" = "#f04d46",
+                    "Venstre" = "#00639E",
+                    "Det Konservative Folkeparti" = "#00583C",
+                    "Lokalliste" = "grey40",
+                    "Radikale Venstre" = "#EC008C",
+                    "Socialistisk Folkeparti" = "#C4151C",
+                    "Alternativet" = "#00FF00",
+                    "Dansk Folkeparti" = "#0F3459")
+
