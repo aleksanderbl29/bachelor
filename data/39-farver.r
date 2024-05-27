644ad954cc71f0d63025bc8ddaad931d1f3ca1ab
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-05-27 13:59:50 +0200

    farver til id af valg

--- data/39-farver.r
+++ data/39-farver.r
@@ -2,4 +2,7 @@ library(tidyverse)
 library(logger)
 
 log_info("Skaber farvepalette til valg")
-valg_farver <-  c("KV2009" = "#E69F00", "KV2013" = "#56B4E9", "KV2017" = "#009E73", "KV2021" = "#F0E442")
+valg_farver <- c("KV2009" = "#E69F00", 
+                 "KV2013" = "#56B4E9",
+                 "KV2017" = "#009E73",
+                 "KV2021" = "#F0E442")
